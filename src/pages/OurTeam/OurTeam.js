14ed498817006ef77394ff06ca8be2077b737b36
Maheshwar098 <107045447+Maheshwar098@users.noreply.github.com>
2023-08-20 10:45:28 +0530

    design fixes

--- src/pages/OurTeam/OurTeam.js
+++ src/pages/OurTeam/OurTeam.js
@@ -9,7 +9,7 @@ const OurTeam = () => {
   return (
     <div className="ourTeamPg">
     <div className="container fade-in">
-      <h2 className="text-white ourTeamTitle text-center fade-in mt-4 mt-sm-3 mb-0 mb-sm-0 fs-1">Who we are</h2>
+      <h2 className="text-white ourTeamTitle text-center fade-in mt-4 mt-sm-3 mb-0 mb-sm-0 fs-1">Our Team</h2>
       <div className="container p-3">
         <div className="container-fluid p-sm-4 p-2 ">
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 gx-0">
